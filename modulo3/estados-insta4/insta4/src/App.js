8767e66ba7eaadad114849f4208e1fd12eae61d9
Mizael costa <raidechin@gmail.com>
2022-04-28 23:34:59 -0300

    exercicio de rederizaçao de lista

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -9,27 +9,33 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+
 class App extends React.Component {
+
+  state = {
+    pessoas: [  {nomeUsuario: 'paulinha',
+    fotoUsuario: 'https://th.bing.com/th/id/OIP.WhLIrKFq8P8X_y1ap7zJoQHaEo?pid=ImgDet&rs=1',
+    fotoPost: 'https://th.bing.com/th/id/OIP.n1DTvH4r4zfK22Qb-E15ogHaEr?pid=ImgDet&rs=1'} ,   { nomeUsuario:'gabriel',
+    fotoUsuario: 'https://saibama.is/wp-content/uploads/2020/12/fases-da-lua-nos-negocios-15531.jpg',
+    fotoPost: 'https://th.bing.com/th/id/OIP.N_STPtWWFUoS7tC4RI2fpgHaHa?pid=ImgDet&w=736&h=736&rs=1' },
+    {
+      nomeUsuario: 'bia',
+      fotoUsuario: 'https://th.bing.com/th/id/R.9f3df38816b146fdd1a14e40de8cd252?rik=uTQw%2fleVrXx3tw&pid=ImgRaw&r=0',
+      fotoPost: 'https://th.bing.com/th/id/OIP.FEgz4gv9z3D_iQZa0lCiRgHaFj?pid=ImgDet&rs=1'
+    }
+  ]
+  }
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://th.bing.com/th/id/OIP.WhLIrKFq8P8X_y1ap7zJoQHaEo?pid=ImgDet&rs=1'}
-          fotoPost={'https://th.bing.com/th/id/OIP.n1DTvH4r4zfK22Qb-E15ogHaEr?pid=ImgDet&rs=1'}
-        />
+         { this.state.pessoas.map((item) =>{
 
-        <Post
-          nomeUsuario={'gabriel'}
-          fotoUsuario={'https://saibama.is/wp-content/uploads/2020/12/fases-da-lua-nos-negocios-15531.jpg'}
-          fotoPost={'https://th.bing.com/th/id/OIP.N_STPtWWFUoS7tC4RI2fpgHaHa?pid=ImgDet&w=736&h=736&rs=1'}
-        />
-
-        <Post
-          nomeUsuario={'bia'}
-          fotoUsuario={'https://th.bing.com/th/id/R.9f3df38816b146fdd1a14e40de8cd252?rik=uTQw%2fleVrXx3tw&pid=ImgRaw&r=0'}
-          fotoPost={'https://th.bing.com/th/id/OIP.FEgz4gv9z3D_iQZa0lCiRgHaFj?pid=ImgDet&rs=1'}
-        />  
+          return(<Post
+          nomeUsuario={item.nomeUsuario}
+          fotoUsuario={item.fotoUsuario}
+          fotoPost={item.fotoPost}
+        />)
+       })}
       </MainContainer>
     );
   }

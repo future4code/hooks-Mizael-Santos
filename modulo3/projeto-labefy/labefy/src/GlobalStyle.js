ac5d08199dfa4fe4eab571236774e2cdf9525c0f
mizael costa santos <raidechin@gmail.com>
2022-05-27 19:46:08 -0300

    labefy

---
+++ modulo3/projeto-labefy/labefy/src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-size: 1rem;
+    margin: 0;
+    padding: 0;
+    font-family:  Arial, Helvetica, sans-serif;
+  }
+`;
+
+export default GlobalStyle; 

2625ac592165f1883a68dd73fc2cf4e177937b28
mizael costa santos <raidechin@gmail.com>
2022-06-14 17:44:59 -0300

    wireframe-projeto-labex

---
+++ modulo4/projeto-labex/projeto-labex/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import axios from "axios";
+import styled from "styled-components";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo4/projeto-labex/projeto-labex/src/pages/HomePage.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Global = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+const AreaHome = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const H1 = styled.h1`
+  font-size: 60px;
+  font-family: sans-serif;
+  padding-bottom: 50px;
+  padding-top: 200px;
+`;
+
+const ButtonFilho1 = styled.button`
+  width: 50%;
+  height: 46px;
+  border: 2px solid black;
+  border-radius: 6px;
+  background-color: blue;
+  color: white;
+  font-size: 18px;
+`;
+
+const ButtonFilho2 = styled.button`
+  width: 50%;
+  height: 46px;
+  border: 2px solid black;
+  border-radius: 6px;
+  background-color: blue;
+  color: white;
+  font-size: 18px;
+`;
+
+const Button1 = styled.div`
+  width: 50%;
+  height: 46px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20px;
+`;
+
+const Button2 = styled.div`
+  width: 50%;
+  height: 46px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function HomePage() {
+  return (
+    <AreaHome>
+      <H1>LabeX</H1>
+
+      <Button1>
+        <ButtonFilho1>Viagens</ButtonFilho1>
+      </Button1>
+
+      <Button2>
+        <ButtonFilho2>Area Admin</ButtonFilho2>
+      </Button2>
+    </AreaHome>
+  );
+}
+
+export default HomePage;

b1b0e4c9a9bccc878f70663a22bf33e5a5af2741
mizael costa santos <raidechin@gmail.com>
2022-07-26 18:46:21 -0300

    exercicio de testes

---
+++ modulo4/intro-testes/testes-unitarios/src/exercicios/ex2.test.js
@@ -0,0 +1,28 @@
+import { checaPalindromo } from "./ex2";
+
+describe("Checa Palíndromo", () => {
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+
+  it("retorna false para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+});
